85693b6c8faeeb19ad6373b407e7582712d18e82
Stéphane Lavergne <lis@imars.com>
2022-03-31 16:17:21 -0400

    Fast: merge both cases, no need to disable inputs

--- js/vp-fast.js
+++ js/vp-fast.js
@@ -8,27 +8,16 @@ import * as $ from './browser.js';
 // Not interfering with touch devices as a precaution.
 // Click event is fired twice, so we're restricting the feature to cases which
 // should not suffer from it.
-
-// Anchors: a second 'click' is harmless
-$.forever('[vp-fast] a[href], a[href][vp-fast]', el => {
-	$.on(el, 'mousedown', ev => {
-		if (ev.button !== 0) return;
-		ev.target.click();
-	});
-});
-
-// Forms: all inputs/buttons disabled after our click event, ahead of
-// Browser's.
 // TODO: is there ANY way to cancel the browser's second click?
-$.forever('form[vp-fast] [type=submit], form [vp-fast][type=submit]', el => {
-	$.on(el, 'mousedown', ev => {
-		const form = ev.target.closest('form');
-		if (!form || ev.button !== 0) return;
-		ev.target.click();
-		$.forall(form, 'button, input, select, textarea', e => {
-			$.set(e, { disabled: true });
+
+$.forever(
+	'[vp-fast] a[href], a[href][vp-fast], form[vp-fast] [type=submit], form [type=submit][vp-fast]',
+	el => {
+		$.on(el, 'mousedown', ev => {
+			if (ev.button !== 0) return;
+			ev.target.click();
 		});
-	});
-});
+	}
+);
 
 export {};
